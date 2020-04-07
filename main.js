d975a3c581785e047578308831b401007b242ff2
ALABI TANTOLUWA HERITAGE <56360427+HERITAGEALABI@users.noreply.github.com>
2020-04-07 02:47:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+//Array of objects
+//Object literalss
+let data= [
+{'principal': 2500, 'time':' 1.8,
+},
+{'principal': 1000, 'time': 5,
+},
+{'principal': 3000, 'time': 1,
+},
+{'principal':2000, 'time':3,
+},
+]
+
+function interestCalculator(arr) {
+    let dataArray = [];
+
+    //If the principal is greater than or equal to 2500 and the time is greater than 1 but less than 3, then rate = 3
+
+    for(let obj of arr){
+        if(obj.principal >= 2500 && (obj.time >=3)) {
+            obj.rate = 3;
+        }
+        
+        //If the principal is greater than or equal to 2500 and  time is greater than or equal to 3, then rate = 4
+
+        else if (obj.principal >= 2500 && obj.time >= 3) {
+            obj.rate = 4;
+        }
+
+        //If the principal is less than 2500 or the time is less than or equal to 1, then  rate = 2w
+
+        else if (obj.principal < 2500 || obj.time <= 1) {
+            obj.rate = 2;
+        }
+
+        //Otherwise, rate = 1;
+        else {
+            obj.rate = 1;
+        }
+        //Calculate the interest for each individual object using the formula: (principal * rate * time) / 100.
+        obj.interest = (obj .principal * obj.rate * obj.time)/100;
+        dataArray.push(obj)
+    }
+    
+//Log the 'interestData' array to console BEFORE your return statement .
+console.log(dataArray)
+
+//
+return dataArray;
+}
+
+//Finally, call/execute the function and the pass the 'data'array you created.
+
+interestCalculator(data)
+
